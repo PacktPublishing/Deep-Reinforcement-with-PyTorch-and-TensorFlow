8d475535e75bec333351b912438ef089a8939445
Leonardo Araujo dos Santos <leonardo_a@apple.com>
2019-04-01 10:04:02 +0100

    Adding working version of sequence to sequence with attention

--- notebooks/recurrent_pytorch/utils_seq_to_seq.py
+++ notebooks/recurrent_pytorch/utils_seq_to_seq.py
@@ -151,3 +151,25 @@ def tensorsFromPair(pair, input_lang, output_lang, device):
     return (input_tensor, target_tensor)
 
 
+def showAttention(input_sentence, output_words, attentions):
+    # Set up figure with colorbar
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(attentions.numpy(), cmap='bone')
+    fig.colorbar(cax)
+
+    # Set up axes
+    ax.set_xticklabels([''] + input_sentence.split(' ') +
+                       ['<EOS>'], rotation=90)
+    ax.set_yticklabels([''] + output_words)
+
+    # Show label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
+
+
+
+
+
